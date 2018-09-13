6e4405920ef4f707a83a5aac855a1b0d1b409a07
cofire <yli_666@163.com>
2018-09-13 20:55:50 +0800

    添加几种设计模式

---
+++ java/designPattern/Business/EJBService.java
@@ -0,0 +1,9 @@
+package Business;
+
+public class EJBService implements BusinessService {
+
+    @Override
+    public void doProcessing() {
+        System.out.println("Processing task by invoking EJB Service");
+    }
+}
