a6f1fb58a2e4adb7c53ad1e171d20388302fb8a1
cofire <yli_666@163.com>
2018-09-17 23:41:54 +0800

    修改启动项

--- Java/sping boot/merckActrl/src/main/java/com/merck/MerckApplication.java
+++ Java/sping boot/merckActrl/src/main/java/com/merck/MerckApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MokeApplication extends SpringBootServletInitializer {
+public class MerckApplication extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
